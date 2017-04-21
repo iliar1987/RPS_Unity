0d430fb937f8ec9ea3a802933f09673563c6e507
ilia <iliaraysin@gmail.com>
2017-04-21 17:36:25 +0300

    working now

--- Assets/ComputeShaders/UpdateRPS.cs
+++ Assets/ComputeShaders/UpdateRPS.cs
@@ -6,8 +6,12 @@ public class UpdateRPS : MonoBehaviour {
 	public ComputeShader m_RPS_compute;
 	private RenderTexture m_texCurrentFrame;
 	private RenderTexture m_texLastFrame;
-	private int width = 256;
-	private int height = 256;
+	private ComputeBuffer m_cbScores;
+	private int width = 1024;
+	private int height = 1024;
+	private float fDiffusionCoeff = 0.5f;
+	private float fDiffusionPower = 1.1f;
+	private float fNormPower = 0.8f;
 
 	public RenderTexture GetCurrentFrameTexture()
 	{
@@ -24,6 +28,11 @@ public class UpdateRPS : MonoBehaviour {
 		m_texLastFrame.enableRandomWrite = true;
 		m_texLastFrame.Create ();
 
+		m_cbScores = new ComputeBuffer (width * height, sizeof(float));
+
+		m_RPS_compute.SetInt ("width", width);
+		m_RPS_compute.SetInt ("height", height);
+
 		Randomize ();
 	}
 	
@@ -34,6 +43,8 @@ public class UpdateRPS : MonoBehaviour {
 //			m_texCurrentFrame.Create ();
 //			RunShader ();
 //		}
+
+		MakeOneStep ();
 	}
 
 	void Randomize()
@@ -42,6 +53,28 @@ public class UpdateRPS : MonoBehaviour {
 		m_RPS_compute.SetTexture (kernelHandle, "Result", m_texCurrentFrame);
 		m_RPS_compute.Dispatch (kernelHandle,width / 8,height / 8,1);
 
+	}
+
+	void MakeOneStep()
+	{
+		m_RPS_compute.SetFloat ("fDiffusionPower", fDiffusionPower);
+		m_RPS_compute.SetFloat ("fDiffusionCoeff", fDiffusionCoeff);
+		m_RPS_compute.SetFloat ("fNormPower", fNormPower);
+
+		int scoresKernel = m_RPS_compute.FindKernel ("CalcScores");
+		m_RPS_compute.SetTexture (scoresKernel, "LastFrame", m_texCurrentFrame);
+		m_RPS_compute.SetBuffer (scoresKernel, "cbScores", m_cbScores);
+		m_RPS_compute.Dispatch(scoresKernel,width / 8,height / 8,1);
+
+		int nextFrameKernel = m_RPS_compute.FindKernel ("CalcNextFrame");
+		m_RPS_compute.SetTexture (nextFrameKernel, "LastFrame", m_texCurrentFrame);
+		m_RPS_compute.SetTexture (nextFrameKernel, "NextFrame", m_texLastFrame);
+		m_RPS_compute.SetBuffer(nextFrameKernel,"cbScores", m_cbScores);
+		m_RPS_compute.Dispatch(nextFrameKernel,width / 8,height / 8,1);
+
+		var temp = m_texLastFrame;
+		m_texLastFrame = m_texCurrentFrame;
+		m_texCurrentFrame = temp;
 	}
 
 }
