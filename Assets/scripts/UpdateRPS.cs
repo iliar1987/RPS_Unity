9850a639b50cbc728f3ce583d4e6e2b7843cd927
ilia <iliaraysin@gmail.com>
2017-04-29 14:12:33 +0300

    1. changed Scores from compute buffer into texture 2. made some modifications to parameters for RPS step

--- Assets/scripts/UpdateRPS.cs
+++ Assets/scripts/UpdateRPS.cs
@@ -6,29 +6,37 @@ public class UpdateRPS : MonoBehaviour {
 	public ComputeShader m_RPS_compute;
 	private RenderTexture m_texCurrentFrame;
 	private RenderTexture m_texLastFrame;
-	private ComputeBuffer m_cbScores;
+	//private ComputeBuffer m_cbScores;
+	private RenderTexture m_texScores;
 	private int width = 1024;
 	private int height = 1024;
 	private float fDiffusionCoeff = 0.5f;
 	private float fDiffusionPower = 1.01f;
-	private float fNormPower = 2.0f;
+	private float fNormPower = 0.9f;
 
 	public RenderTexture GetCurrentFrameTexture()
 	{
 		return m_texCurrentFrame;
+		//return m_texScores;
 	}
 
 	// Use this for initialization
 	void Start () {
 		m_texCurrentFrame = new RenderTexture (width,height,0);
+		m_texCurrentFrame.format = RenderTextureFormat.ARGB32;
 		m_texCurrentFrame.enableRandomWrite = true;
 		m_texCurrentFrame.Create ();
 
 		m_texLastFrame = new RenderTexture (width,height,0);
+		m_texLastFrame.format = RenderTextureFormat.ARGB32;
 		m_texLastFrame.enableRandomWrite = true;
 		m_texLastFrame.Create ();
 
-		m_cbScores = new ComputeBuffer (width * height, sizeof(float));
+		//m_cbScores = new ComputeBuffer (width * height, sizeof(float));
+		m_texScores = new RenderTexture(width,height,0);
+		m_texScores.format = RenderTextureFormat.RFloat;
+		m_texScores.enableRandomWrite = true;
+		m_texScores.Create ();
 
 		m_RPS_compute.SetInt ("width", width);
 		m_RPS_compute.SetInt ("height", height);
@@ -74,13 +82,14 @@ public class UpdateRPS : MonoBehaviour {
 
 		int scoresKernel = m_RPS_compute.FindKernel ("CalcScores");
 		m_RPS_compute.SetTexture (scoresKernel, "LastFrame", m_texCurrentFrame);
-		m_RPS_compute.SetBuffer (scoresKernel, "cbScores", m_cbScores);
+		m_RPS_compute.SetTexture (scoresKernel, "Scores", m_texScores);
+		//m_RPS_compute.SetBuffer (scoresKernel, "cbScores", m_cbScores);
 		m_RPS_compute.Dispatch(scoresKernel,width / 8,height / 8,1);
 
 		int nextFrameKernel = m_RPS_compute.FindKernel ("CalcNextFrame");
 		m_RPS_compute.SetTexture (nextFrameKernel, "LastFrame", m_texCurrentFrame);
 		m_RPS_compute.SetTexture (nextFrameKernel, "NextFrame", m_texLastFrame);
-		m_RPS_compute.SetBuffer(nextFrameKernel,"cbScores", m_cbScores);
+		m_RPS_compute.SetTexture(nextFrameKernel,"Scores", m_texScores);
 		m_RPS_compute.Dispatch(nextFrameKernel,width / 8,height / 8,1);
 
 		var temp = m_texLastFrame;
